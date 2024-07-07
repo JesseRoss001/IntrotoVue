a7730bf14e263174abe6b6185741c265a36cdb16
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-07-07 14:20:11 +0100

    first thing

---
+++ my-vue-app/src/components/ToDoList.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-text-field
+          v-model="newTodo"
+          label="New To-Do"
+          @keyup.enter="addTodo"
+        ></v-text-field>
+        <v-btn @click="addTodo">Add</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-list>
+          <v-list-item
+            v-for="(todo, index) in todos"
+            :key="index"
+          >
+            <v-list-item-content>{{ todo }}</v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon @click="removeTodo(index)">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push(this.newTodo.trim())
+        this.newTodo = ''
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
